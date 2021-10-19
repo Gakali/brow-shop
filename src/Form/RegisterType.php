e70fe5967f6885a309fe32ef5bd580ccc48b0f8e
Brow <igakali@icloud.com>
2021-10-19 12:06:46 +0200

    register form / register View

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class,[
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prénom'
+                ]
+            ] )
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir le Nom '
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre email'
+                    ]
+            ])
+            ->add('password', RepeatedType::class,[
+            'type'=> PasswordType::class,
+            'invalid_message' => ' les mots de passes doivent être identiques',
+            'label' => 'Votre mot de passe ',
+            'required' => true,
+            'first_options' => ['label' =>'Mot de passe'],
+            'second_options' => ['label' => 'Confirmez le mot de passe']])
+            
+
+             ->add ('submit', SubmitType::class ,[
+                 'label' => 'S\'inscrire'
+             ])
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
