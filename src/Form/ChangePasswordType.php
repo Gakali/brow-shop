ff6a50e3dbe3b5394da67f599e588c39b2f1b7a3
Brow <igakali@icloud.com>
2021-11-18 12:20:24 +0100

    finalisation espace mdp

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -7,8 +7,10 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ChangePasswordType extends AbstractType
@@ -28,29 +30,30 @@ class ChangePasswordType extends AbstractType
                 'disabled' => true,
                 'label' => 'Mon Prénom'
             ])
-            ->add('password', PasswordType::class, [ 
+            ->add('old_password', PasswordType::class, [ 
                 'label' => 'Mon mot de passe actuel',
+                'mapped' => false,
                 'attr' => [
-                'placeholder' => 'Veuillez saisir un nouveau mot de passe ']
+                'placeholder' => 'Veuillez saisir votre mot de passe  ']
             ])
             ->add('new_password', RepeatedType::class,[
-                // 'constraints' => new Length([
-                //     'min' => 4,
-                //     'max' => 30,
-                //     'minMessage' => 'Vous devez saisir au moins 4 caractères',
-                //     'maxMessage' => 'Vous devez saisir au maximum 30 caractères',
-                // ]),
+                
                 'type'=> PasswordType::class,
                 'mapped' => false,
+
                 'invalid_message' => ' les mots de passes doivent être identiques',
                 'label' => 'Mon mot de passe ',
                 'required' => true,
+
                 'first_options' => ['label' =>'Mot de passe',
                 'attr' =>['placeholder' => 'Merci de saisir un mot de passe']],
+
                 'second_options' => ['label' => 'Confirmez le mot de passe',
                 'attr' =>['placeholder' => 'Confirmez le mot de passe']
                 ]])
-
+            ->add ('submit', SubmitType::class ,[
+                    'label' => 'Confirmer'
+                ])
                 
         ;
     }
