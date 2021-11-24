221f881e101283fc90bbde88bf0a22413ca561e6
Brow <igakali@icloud.com>
2021-11-24 12:45:37 +0100

    Ajout de produits dans le dash

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -48,7 +48,7 @@ class Product
     private $price;
 
     /**
-     * @ORM\ManyToOne(targetEntity=category::class, inversedBy="products")
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="products")
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
