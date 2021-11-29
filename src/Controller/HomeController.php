0b0605b6348b4cf845fed9095b841c38e705a5d4
Brow <igakali@icloud.com>
2021-11-29 07:10:33 +0100

    panier v1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,8 +12,16 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        $session->set('basket',[
+            [
+                'id' => 522,
+                'quantity' => 12
+            ]
+        ]);
+        $basket = $session->get('basket');
+        dd($basket);
         return $this->render('home/index.html.twig');
     }
 }
